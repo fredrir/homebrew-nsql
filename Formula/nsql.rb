d5e66470488991a7fd195db475be78d99aca2953
fredrir <fhansteen@gmail.com>
2026-06-29 21:24:38 +0200

    Add nsql formula

---
+++ Formula/nsql.rb
@@ -0,0 +1,11 @@
+class Nsql < Formula
+  desc "TODO: short description of nsql"
+  homepage "https://github.com/fredrir/nsql"
+  url "https://github.com/fredrir/nsql/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "TODO_SHA256"
+  license "MIT"
+
+  def install
+    bin.install "nsql"
+  end
+end
